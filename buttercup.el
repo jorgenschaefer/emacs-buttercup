7fa79c407a6a53f2bcda47673a2a5c95d99554b6
Jorgen Schaefer <contact@jorgenschaefer.de>
2015-04-05 16:14:40 +0200

    Expand and improve the package commentary.

--- buttercup.el
+++ buttercup.el
@@ -20,6 +20,18 @@
 
 ;;; Commentary:
 
+;; Buttercup is a behavior-driven development framework for testing
+;; Emacs Lisp code. It is heavily inspired by the Jasmine test
+;; framework for JavaScript.
+
+;; A test suite begins with a call to the Buttercup macro `describe` with
+;; the first parameter describing the suite and the rest being the body
+;; of code that implements the suite.
+
+;; (describe "A suite"
+;;   (it "contains a spec with an expectation"
+;;     (expect t :to-be t)))
+
 ;; The ideas for project were shamelessly taken from Jasmine
 ;; <https://jasmine.github.io>.
 
