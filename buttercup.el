ba62f80555d46faf49dc451c0ad20f39f6a170ab
Ola Nilsson <ola.nilsson@gmail.com>
2022-04-10 17:57:42 +0200

    Bump version: 1.24 → 1.25

--- buttercup.el
+++ buttercup.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2015-2017  Jorgen Schaefer <contact@jorgenschaefer.de>
 
-;; Version: 1.24
+;; Version: 1.25
 ;; Author: Jorgen Schaefer <contact@jorgenschaefer.de>
 ;; Maintainer: Ola Nilsson <ola.nilsson@gmail.com>
 ;; Package-Requires: ((emacs "24.3"))
