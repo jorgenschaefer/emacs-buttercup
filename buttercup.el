21ded874f7af380b593776eac06a87cf7f7ae439
Ola Nilsson <ola.nilsson@gmail.com>
2022-12-27 19:57:45 +0100

    Remove dead code from :to-throw matcher
    expected-signal-symbol is never unset after line 1 of the function.

--- buttercup.el
+++ buttercup.el
@@ -598,10 +598,6 @@ See also `buttercup-define-matcher'."
         (format "Probable incorrect use of `:to-throw' matcher: pass an expression instead of a function: `%S'"
                 unevaluated-expr)
         'yellow)))
-    ;; If no signal specificaiton, use `error' as the signal symbol
-    (when (and (null expected-signal-symbol)
-               (null expected-signal-args))
-      (setq expected-signal-symbol 'error))
     ;; Set the above 4 variables
     (condition-case err
         (setq expr-value
@@ -2128,9 +2124,11 @@ ARG is ‘toggle’; disable otherwise."
            (add-hook 'after-load-functions #'buttercup-check-for-stale-elc)))
         (t (remove-hook 'after-load-functions #'buttercup-check-for-stale-elc))))
 
+(provide 'buttercup)
+;;; buttercup.el ends here
+
 ;; Local Variables:
 ;; indent-tabs-mode: nil
 ;; sentence-end-double-space: nil
+;; tab-width: 8
 ;; End:
-(provide 'buttercup)
-;;; buttercup.el ends here
