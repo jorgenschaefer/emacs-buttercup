cc5a2ab7c7f18aaaf525fac61fe59bae5ad018dd
Ola Nilsson <ola.nilsson@gmail.com>
2025-08-01 02:01:38 +0200

    Bump version: 1.37 → 1.38

--- buttercup.el
+++ buttercup.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015-2017  Jorgen Schaefer <contact@jorgenschaefer.de>
 ;; Copyright (C) 2018-2025  Ola Nilsson <ola.nilsson@gmail.com>
 
-;; Version: 1.37
+;; Version: 1.38
 ;; Author: Jorgen Schaefer <contact@jorgenschaefer.de>
 ;; Maintainer: Ola Nilsson <ola.nilsson@gmail.com>
 ;; Package-Requires: ((emacs "24.4"))
